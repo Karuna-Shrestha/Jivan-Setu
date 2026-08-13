1be7ab7ad35ef2ea535597270fe44d4f69fac7d2
karuna360600-ops <karuna360600@gmail.com>
2026-08-13 07:16:45 +0545

    updated

--- Backend/BloodDonationBackend/src/config/db.js
+++ Backend/BloodDonationBackend/src/config/db.js
@@ -1,20 +1,15 @@
 import mysql from "mysql2";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "blood_donation",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-db.connect((err) => {
-  if (err) {
-    console.log("Error while connecting database");
-    console.error(err);
-    return;
-  }
-
-  console.log(" Database connected successfully");
-});
 
 export default db;
